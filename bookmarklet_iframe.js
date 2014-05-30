9cce396296ec43a95b9984ac0fd651ee95051870
Gene Arnold <HomeBase@Genes-MacBook-Pro-3.local>
2014-05-30 13:43:15 -0400

    Heading Change

--- bookmarklet_iframe.js
+++ bookmarklet_iframe.js
@@ -1,9 +1,33 @@
-//
-//Code for bookmarklet
-//
-// javascript:(function(){if(window.myBookmarklet!==undefined){myBookmarklet();}else{document.body.appendChild(document.createElement('script')).src='http://localhost/~HomeBase/bookmarklets/bookmarklet_iframe.js?';}})();
-//
-
+/*
+ * ========================================================================
+ * bookmarklet_iframe.js : v0.0.1
+ * 
+ * ========================================================================
+ * Copyright 2014 
+ * Author: Gene Arnold 
+ *
+ * https://github.com/GeneArnold/Bookmarklet-Examples
+ *
+ * Unless you have purchased a commercial license agreement from Jaspersoft Inc., the following license terms apply:
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the 
+ * GNU Affero General Public License as published by the Free Software Foundation, either version 3 
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public 
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License 
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ * ======================================================================== 
+ *
+ * Code for bookmarklet:
+ * javascript:(function(){if(window.myBookmarklet!==undefined){myBookmarklet();}else{document.body.appendChild(document.createElement('script')).src='http://localhost/~HomeBase/bookmarklets/bookmarklet_iframe.js?';}})();
+ *
+ *
+ *
+ */
 
 (function(){
 
