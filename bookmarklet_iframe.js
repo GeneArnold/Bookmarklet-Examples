cba37ec2679746fd8ef7c7477d67d3a29728b8dc
Gene Arnold <HomeBase@Genes-MacBook-Pro-3.local>
2014-05-30 13:20:15 -0400

    First commit
    First commit

---
+++ bookmarklet_iframe.js
@@ -0,0 +1,85 @@
+//
+//Code for bookmarklet
+//
+// javascript:(function(){if(window.myBookmarklet!==undefined){myBookmarklet();}else{document.body.appendChild(document.createElement('script')).src='http://localhost/~HomeBase/bookmarklets/bookmarklet_iframe.js?';}})();
+//
+
+
+(function(){
+
+	// the minimum version of jQuery we want
+	var v = "1.3.2";
+	var jq;
+		
+	// check for jQuery. if it exists, verify it's not too old.
+	if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
+		var done = false;
+		var script = document.createElement("script");
+		script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
+		script.onload = script.onreadystatechange = function(){
+			if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
+				done = true;
+				initMyBookmarklet();
+			}
+		};
+		document.getElementsByTagName("head")[0].appendChild(script);
+	} else {
+		initMyBookmarklet();
+	}
+	
+	function initMyBookmarklet() {
+		var jq = window.jQuery.noConflict();
+		(window.myBookmarklet = function() {
+			function getSelText() {
+				var s = '';
+				if (window.getSelection) {
+					s = window.getSelection();
+				} else if (document.getSelection) {
+					s = document.getSelection();
+				} else if (document.selection) {
+					s = document.selection.createRange().text;
+				}
+				return s;
+			}
+			if (jq("#theFrame").length == 0) {
+				var s = "";
+				s = getSelText();
+				if (s == "") {
+					var s = prompt("Forget?");
+				}
+				if ((s != "") && (s != null)) {
+					//alert(s);
+					jq("body").append("\
+					<div id='theFrame'>\
+						<div id='theFrame_veil' style=''>\
+							<p style='position: fixed; top: 60%; left: 40%;'>Loading...</p>\
+						</div>\
+						<iframe style='background-color:white' scrolling='no' src='http://localhost:8080/jasperserver-pro56_Beta/flow.html?_flowId=viewReportFlow&reportUnit=/public/Samples/VisualizeJS/ProductDetails&parProductId=" + s + "&viewAsDashboardFrame=true' onload=\"window.jQuery('#theFrame iframe').slideDown(500);\">Enable iFrames.</iframe>\
+						<style type='text/css'>\
+							#theFrame_veil { display: none; position: fixed; width: 100%; height: 100%; top: 0; left: 0; background-color: rgba(255,255,255,.6); cursor: pointer; z-index: 900; }\
+							#theFrame_veil p { color: black; font: normal normal bold 20px/20px Helvetica, sans-serif; position: absolute; top: 50%; left: 50%; width: 10em; margin: -10px auto 0 -5em; text-align: center; }\
+							#theFrame iframe { display: none; position: fixed; top: 25%; left: 35%; width: 300px; height: 400px; z-index: 999; border: 5px solid rgba(0,0,0,.5); margin: 0 0 0 0; }\
+						</style>\
+					</div>");
+					jq("#theFrame_veil").fadeIn(750);
+				}
+			} else {
+				jq("#theFrame_veil").fadeOut(750);
+				jq("#theFrame iframe").slideUp(500);
+				var elem = document.getElementById("theFrame");
+				//elem.remove();
+				//setTimeout("jq('#theFrame').remove()", 750);
+				setTimeout(elem.remove(), 750);
+			}
+			jq("#theFrame_veil").click(function(event){
+				jq("#theFrame_veil").fadeOut(750);
+				jq("#theFrame iframe").slideUp(500);
+				var elem = document.getElementById("theFrame");
+				//elem.remove();
+				//setTimeout("jq('#theFrame').remove()", 750);
+				setTimeout(elem.remove(), 750);
+			});
+		})();
+	}
+
+})();
