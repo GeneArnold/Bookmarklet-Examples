81f17214214f32fe9903eb013883cd50be4aa955
Gene Arnold <HomeBase@Genes-MacBook-Pro-3.local>
2014-06-23 07:35:35 -0400

    URL Changes

--- bookmarklet_visualize.js
+++ bookmarklet_visualize.js
@@ -23,7 +23,7 @@
  * ======================================================================== 
  *
  * Code for bookmarklet:
- * javascript:(function(){if(window.myBookmarklet!==undefined){myBookmarklet();}else{document.body.appendChild(document.createElement('script')).src='http://localhost/~HomeBase/bookmarklets/bookmarklet_iframe.js?';}})();
+ * javascript:(function(){if(window.myBookmarklet!==undefined){myBookmarklet();}else{document.body.appendChild(document.createElement('script')).src='http://localhost/~HomeBase/bookmarklets/bookmarklet_visualize.js?';}})();
  *
  * The report for this example can be found here
  * https://github.com/GeneArnold/Github-Reports/blob/master/product_details.jrxml
@@ -34,6 +34,19 @@
 	// the minimum version of jQuery we want
 	var v = "1.3.2";
 	var jq;
+
+	// check for jQuery. if it exists, verify it's not too old.
+	if (window.visualize === undefined) {
+		var done = false;
+		var script = document.createElement("script");
+		script.src = "http://localhost:8080/jasperserver-pro/client/visualize.js";
+		script.onload = script.onreadystatechange = function(){
+			//if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
+			//	done = true;
+			//}
+		};
+		document.getElementsByTagName("head")[0].appendChild(script);
+	}
 		
 	// check for jQuery. if it exists, verify it's not too old.
 	if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
@@ -78,14 +91,40 @@
 						<div id='theFrame_veil' style=''>\
 							<p style='position: fixed; top: 60%; left: 40%;'>Loading...</p>\
 						</div>\
-						<iframe style='background-color:white' scrolling='no' src='http://localhost:8080/jasperserver-pro/flow.html?_flowId=viewReportFlow&j_username=jasperadmin&j_password=jasperadmin&reportUnit=/public/Samples/Reports/ProductDetails&parProductId=" + s + "&viewAsDashboardFrame=true' onload=\"window.jQuery('#theFrame iframe').slideDown(500);\">Enable iFrames.</iframe>\
+						<div id='report' style='background-color:white' scrolling='no' onload=\"window.jQuery('#theFrame <div>').slideDown(500);\"></div>\
 						<style type='text/css'>\
 							#theFrame_veil { display: none; position: fixed; width: 100%; height: 100%; top: 0; left: 0; background-color: rgba(255,255,255,.6); cursor: pointer; z-index: 900; }\
 							#theFrame_veil p { color: black; font: normal normal bold 20px/20px Helvetica, sans-serif; position: absolute; top: 50%; left: 50%; width: 10em; margin: -10px auto 0 -5em; text-align: center; }\
-							#theFrame iframe { display: none; position: fixed; top: 25%; left: 35%; width: 300px; height: 400px; z-index: 999; border: 5px solid rgba(0,0,0,.5); margin: 0 0 0 0; }\
+							#report { position: fixed; top: 25%; left: 35%; width: 300px; height: 400px; z-index: 999; border: 5px solid rgba(0,0,0,.5); margin: 0 0 0 0; }\
 						</style>\
 					</div>");
 					jq("#theFrame_veil").fadeIn(750);
+
+					visualize({
+		                auth: {
+		                    name: "jasperadmin",
+		                    password: "jasperadmin",
+		                    organization: "organization_1"
+		                }
+		            }, function (v) {
+		                var report = v.report({
+		                   resource: "/public/Samples/Reports/ProductDetails",
+		                   container: "#report",
+		                   params: { "parProductId": [s.toString()] },
+		                   events:{
+		                        beforeRender: function(el){
+		                            // find all spans
+		                            jq(el).find("td").each(function() {
+		                                var $this = jq(this);
+		                                $this.html($this.html().replace(/&nbsp;/g, ''));
+		                            });
+		                        }
+		                    },
+		                   error: function(err) {
+		                        alert(err.message);
+		                   } 
+		                });
+		            });
 				}
 			} else {
 				jq("#theFrame_veil").fadeOut(750);
